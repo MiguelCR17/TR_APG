30f8ddbe70ed84bdd55b6e5b1ad1e708e4bacb25
Miguel Ribeiro <mcribeiro1710@gmail.com>
2021-12-20 17:03:08 +0000

    ja a 2ª opção ja esta a funcionar mas temos de arranjar uma nova maneira devolta atras para o menu pq assim n da e dps meter os calculos e fazer alguns extras

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 void menu();
 void ler_ficheiro();
+FILE *fp;
 void tabela();
-
 
 int main() {
     menu();
@@ -30,30 +30,27 @@ void menu(){
 void ler_ficheiro() {
     char file[200];
     char volta;
-    int x = 19;
     printf("Para escolher o ficheiro escreva o diretorio do ficheiro:\n");
-    fflush(stdin);//o entar fica na memoria entao isto "apaga a memoria" para fazer o if direito
+    fflush(stdin);                    //o entar fica na memoria entao isto "apaga a memoria" para fazer o if direito
     gets(file);
-    FILE *fp = fopen(file, "r");
-    if (fp != NULL) {
-        while (!feof(fp)) {
-            printf("%c", fgetc(fp));              //printf("%c", fgetc(fp));
-        }
-        while(volta != 1) {
-            printf("\nDeseja voltar para o menu? Digite '1'\n");
-            scanf("%d", &volta);
-            if(volta == 1){
-                menu();
+    fp = fopen(file, "r");
+        if (fp != NULL) {
+            printf("Ficheiro lido com sucesso!\n");
+            while (volta != 1) {
+                printf("\nDeseja voltar para o menu? Digite '1'\n");
+                scanf("%d", &volta);
+                if (volta == 1) {
+                    menu();
+                }
             }
+            fclose(fp);
+        } else {
+            printf("\nErro na abertura do ficheiro!\n");
         }
-        fclose(fp);
-    }
-    else {
-        printf("\nErro na abertura do ficheiro!\n");
-    }
 }
 
-void tabela(fp){
-    fflush(stdin);
-    printf("%d", fp);
+void tabela(){
+    while (!feof(fp)) {
+        printf("%c", fgetc(fp));                                           //printf("%c", fgetc(fp));
+    }
 }
