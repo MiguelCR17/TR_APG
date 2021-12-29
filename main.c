ede890ba1b4c3060969d68e732e6f24307630efa
Miguel Ribeiro <mcribeiro1710@gmail.com>
2021-12-29 00:23:15 +0000

    vamos manter esta versao fiz umas alterações mas o problema é que como isto esta agr n da para fazer contas pq temos de por numa string ou algo do estilo para poder trabalhar com os valores

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void menu();
-void ler_ficheiro();
+char *ler_ficheiro();
 FILE *fp;
-void tabela();
+void tabela(char *file);
+char file[200];
 
 int main() {
     menu();
@@ -14,43 +16,80 @@ int main() {
 //***************************************************MENU***************************************************************
 void menu(){
     char menu;
-    printf("Digite 1 para ler o ficheiro, 2 para ver a tabela:\n");
+    opcao:printf("Digite 1 para ler o ficheiro, 2 para ver a tabela:\n");
     scanf("%d", &menu);
 
     switch(menu) {
         case 1: //ler ficheiro
             ler_ficheiro();
-            break;
+            goto opcao;
         case 2: //mostrar tabela
-            tabela();
-            break;
+            tabela(file);
+            goto opcao;
     }
 }
 //**************************************Ler ficheiro********************* C:\Users\mcrib\OneDrive\Documentos\example.csv
-void ler_ficheiro() {
-    char file[200];
-    char volta;
+char *ler_ficheiro() {
     printf("Para escolher o ficheiro escreva o diretorio do ficheiro:\n");
     fflush(stdin);                    //o entar fica na memoria entao isto "apaga a memoria" para fazer o if direito
     gets(file);
+    printf("%s\n",file);
     fp = fopen(file, "r");
-        if (fp != NULL) {
-            printf("Ficheiro lido com sucesso!\n");
-            while (volta != 1) {
-                printf("\nDeseja voltar para o menu? Digite '1'\n");
-                scanf("%d", &volta);
-                if (volta == 1) {
-                    menu();
-                }
-            }
-            fclose(fp);
-        } else {
-            printf("\nErro na abertura do ficheiro!\n");
-        }
+    if (fp != NULL) {
+        printf("Ficheiro lido com sucesso!\n");
+        fclose(fp);
+    } else {
+        printf("\nErro na abertura do ficheiro!\n");
+    }
+return file;
 }
 
-void tabela(){
-    while (!feof(fp)) {
-        printf("%c", fgetc(fp));                                           //printf("%c", fgetc(fp));
+void tabela(char *file){
+    char buffer[80];
+    //abrir ficheiro
+    printf("Ficheiro %s lido com sucesso!\n",file);
+
+    fp = fopen(file, "r");
+
+    if(fp == NULL) {
+        printf("\nErro na abertura do ficheiro!\n");
     }
+    int i=0;
+
+    while( fgets (buffer, 60, fp)!=NULL ) {
+        /* writing content to stdout */
+        puts(buffer);
+        i++;
+    }
+    printf("%d", i);
+    fclose(fp);
+
+    //ler a 1º linha
+    //ler 1º coluna
+    //fechar ficheiro
+    //imprimir
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    while (!feof(fp)) {
+       // printf("%c", fgetc(fp));                                           //printf("%c", fgetc(fp));
+        char token = strtok(buffer, ";");
+        while (token) {
+            // Just printing each integer here but handle as needed
+            printf("%s\n", token);
+
+            token = strtok(NULL, ";");
+        }
+    }
+    */
 }
